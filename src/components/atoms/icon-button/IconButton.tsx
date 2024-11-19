be8fa214b7fc1df6a92e0ea8fee3db2e990b137a
noumantahir <nouman.hawkdev@gmail.com>
2024-11-19 23:43:41 +0500

    added basic icon button

--- src/components/atoms/icon-button/IconButton.tsx
+++ src/components/atoms/icon-button/IconButton.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/Entypo'; // Use your preferred icon library
+import styles from './IconButton.styles';
 
-export const IconButton = () => {
+interface Props {
+  iconName:string;
+  disabled?:boolean;
+  onPress:()=>void;
+}
+
+
+export const IconButton = ({iconName, disabled, onPress}:Props) => {
   return (
-    <View>
-      <Text>IconButton</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled}
+      style={styles.iconButton}
+    >
+      <Icon name={iconName} size={32} color={'#000'} />
+    </TouchableOpacity>
+
   )
 }
