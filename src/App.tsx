a9ac2bcfb00ca6072d4273d88fe9266c71b49e3c
noumantahir <nouman.hawkdev@gmail.com>
2024-11-19 12:05:53 +0500

    add services placholders

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { TranscriptScreen } from "./screens";
+
+
+
+const App = () => {
+
+
+    //NOTE: This is where router and othere store related initialisations can be referenced
+    //but for demo only referencing to single trasncript screen
+    return <TranscriptScreen />
+}
+
+export default App;
