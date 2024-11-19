00ce0631023b3036bc5197c03c1796d119effbe2
noumantahir <nouman.hawkdev@gmail.com>
2024-11-20 00:08:29 +0500

    make control panel functional

---
+++ src/components/molecules/control-panel/ControlPanel.styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+      justifyContent: 'flex-end', 
+      alignItems: 'center',      
+      backgroundColor: '#f9f9f9', 
+      borderTopWidth:1,
+      borderColor: '#ddd',        
+    },
+    controlPanel: {
+      flexDirection: 'row',
+      alignItems:'center',
+      justifyContent: 'space-around', 
+      paddingTop: 16,        
+      backgroundColor: '#fff',    
+    },
+  
+  });
