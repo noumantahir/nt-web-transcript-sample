a63932459c3a997973aabd0199c61622b5b03c6e
noumantahir <nouman.hawkdev@gmail.com>
2024-11-19 11:16:36 +0500

    integrated and test react-native-web

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {createRoot} from 'react-dom/client';
+import {Platform} from 'react-native';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (Platform.OS === 'web') {
+    const root = createRoot(document.getElementById('root'));
+    root.render(<App />);
+  }
