b01448828624e5a9d52249d9de348c4ad06e63bb
noumantahir <nouman.hawkdev@gmail.com>
2024-11-19 12:06:57 +0500

    added molecule components placholders

---
+++ src/components/molecules/control-panel/ControlPanel.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export const ControlPanel = () => {
+  return (
+    <View>
+      <Text>ControlPanel</Text>
+    </View>
+  )
+}
