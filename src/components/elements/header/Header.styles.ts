bf08a42345b83aa77bf07101fde3271933c7b4cd
noumantahir <nouman.hawkdev@gmail.com>
2024-11-20 18:50:17 +0500

    matcher header with figma ui

--- src/components/elements/header/Header.styles.ts
+++ src/components/elements/header/Header.styles.ts
@@ -1,13 +1,19 @@
-import { StyleSheet } from "react-native";
+import { Platform, StyleSheet } from "react-native";
+import { Colors } from "theme";
 
 export default StyleSheet.create({
     container: {
       width: '100%',
+      height: Platform.select({
+        android:56,
+        ios:44,
+        web:60
+      }),
       paddingVertical: 15,
       alignItems: 'center',
       justifyContent: 'center',
       borderBottomWidth: 1,
-      borderBottomColor: '#ddd',
+      borderBottomColor: Colors.border,
     },
   });
   
