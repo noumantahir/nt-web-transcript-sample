db0c8f342a9de6ac092ce3d0d5ec1e8b31ae4828
noumantahir <nouman.hawkdev@gmail.com>
2024-11-20 23:09:09 +0500

    delayed animation support in icon button

--- src/components/atoms/icon-button/IconButton.tsx
+++ src/components/atoms/icon-button/IconButton.tsx
@@ -1,27 +1,51 @@
-import { TouchableOpacity } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/Entypo'; // Use your preferred icon library
+import { Platform, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import Icon from 'react-native-vector-icons/Entypo'; 
 import styles from './IconButton.styles';
 import { Colors } from 'theme';
-
+import Animated, { BounceIn } from 'react-native-reanimated';
 interface Props {
   iconName: string;
   disabled?: boolean;
   size?: number;
   color?: string;
+  animateWithDelay?: number;
   onPress: () => void;
 }
 
 
-export const IconButton = ({ iconName, disabled, size, color, onPress }: Props) => {
+export const IconButton = ({ iconName, disabled, size, color, animateWithDelay, onPress }: Props) => {
+
+  const isWeb = Platform.OS === 'web';
+  const [visible, setVisible] = useState(!isWeb || !animateWithDelay);
+
+  //handles web delayed animation glitch issue
+  if (isWeb && animateWithDelay) {
+    setTimeout(() => {
+      setVisible(true);
+    }, animateWithDelay + 20) //extra delay to avoid momentary glitch
+  }
+
+  const animation = animateWithDelay !== undefined
+    ? BounceIn.delay(animateWithDelay)
+    : undefined;
+
+  const _wrapperStyle = {
+    ...styles.iconButton, 
+    opacity: visible ? 1 : 0 
+  }
+
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      disabled={disabled}
-      style={styles.iconButton}
-    >
-      <Icon name={iconName} size={size || 24} color={color || Colors.textBlack} />
-    </TouchableOpacity>
+    <Animated.View entering={animation}>
+      <TouchableOpacity
+        onPress={onPress}
+        disabled={disabled}
+        style={_wrapperStyle}
+      >
+        <Icon name={iconName} size={size || 24} color={color || Colors.textBlack} />
+      </TouchableOpacity>
+    </Animated.View>
+
 
   )
 }
